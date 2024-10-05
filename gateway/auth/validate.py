f6e182e5d9f0f115560c4cff045eab72a69705d9
Saurabh9542 <saurabhverma9542@gmail.com>
2024-10-05 17:25:49 +0530

    API GateWay in progress!

---
+++ gateway/auth/validate.py
@@ -0,0 +1,23 @@
+import os, requests
+
+
+def token(request):
+    if not "Authorization" in request.headers:
+        return None, ("misssing credentials", 401)
+    
+    token = request.headers["Authorization"]
+
+
+    if not token:
+        return None, ("misssing credentials", 401)
+
+    response = requests.post(
+        f"http://{os.environ.get("AUTH_SVC_ADDRESS")}/validate",
+        headers={"Authorization": token},
+    )
+
+    if response.status_code == 200:
+        return response.txt, None
+    
+    else:
+        return None, (response.txt, status_code)
